a7af233df861e8a6319bb02223c4514ab932d86f
202110822 <95172240+ddxyxxn@users.noreply.github.com>
2024-09-13 22:51:08 +0900

    사진 촬영 및 이미지 띄우기

--- app/src/main/java/com/example/z_project/MainActivity.kt
+++ app/src/main/java/com/example/z_project/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.example.z_project
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.os.PersistableBundle
-import android.provider.MediaStore
-import android.view.View
-import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
@@ -24,14 +18,10 @@ import com.example.z_project.mypage.MypageFragment
 import com.example.z_project.record.RecordFragment
 import com.example.z_project.upload.UploadFragment
 import com.example.z_project.mypage.LogoutFragment
-import java.io.File
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    private lateinit var imageCaptureLauncher: ActivityResultLauncher<Intent>
-    private lateinit var currentPhotoUri: Uri
-    private lateinit var imageView: ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,44 +30,7 @@ class MainActivity : AppCompatActivity() {
 
         initBottomNavigation()
 
-        imageView = findViewById(R.id.imageView) // XML에서 ImageView 참조
-        // ActivityResultLauncher 초기화
-        imageCaptureLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val bitmap = BitmapFactory.decodeFile(currentPhotoUri.path)
-                imageView.setImageBitmap(bitmap)
-            }
-        }
-
     }
-
-    @Throws(IOException::class)
-    fun capturePhoto(view: View) {
-        // 카메라 앱 호출을 위한 Intent 생성
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (takePictureIntent.resolveActivity(packageManager) != null) {
-            // 이미지 파일 생성
-            val imageFile: File? = createImageFile()
-            imageFile?.also {
-                // 이미지가 저장될 파일의 URI 생성
-                currentPhotoUri = FileProvider.getUriForFile(
-                    this,
-                    "com.example.myapp.fileprovider", // FileProvider authority
-                    it
-                )
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, currentPhotoUri)
-                // 카메라 인텐트를 ActivityResultLauncher로 실행
-                imageCaptureLauncher.launch(takePictureIntent)
-            }
-        }
-    }
-    @Throws(IOException::class)
-    private fun createImageFile(): File? {
-        // 이미지 파일 생성 로직을 구현
-        val storageDir: File? = getExternalFilesDir(null)
-        return File.createTempFile("IMG_", ".jpg", storageDir)
-    }
-
 
     private fun initBottomNavigation(){
 
@@ -125,9 +78,4 @@ class MainActivity : AppCompatActivity() {
         transaction.commit()
     }
 
-//    btn1.setOnClickListener {
-//        supportFragmentManager.beginTransaction()
-//            .replace(R.id.view, LoginFragment())
-//            .commit()
-//    }
 }
--- app/src/main/java/com/example/z_project/upload/UploadFragment.kt
+++ app/src/main/java/com/example/z_project/upload/UploadFragment.kt
@@ -11,13 +11,10 @@ import android.app.Activity
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-
-
 import android.net.Uri
 import android.os.Environment
 import androidx.core.content.FileProvider
 import java.io.File
-
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -26,57 +23,27 @@ import android.graphics.BitmapFactory
 import android.graphics.Camera
 import android.os.Build
 import android.provider.MediaStore
+import android.widget.Button
 import android.widget.ImageView
-
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.z_project.R
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Locale
-
+import androidx.appcompat.app.AppCompatActivity
+import com.example.z_project.databinding.ActivityMainBinding
+import java.io.FileOutputStream
 
 class UploadFragment : Fragment() {
+    lateinit var binding: FragmentUploadBinding
 
-    private lateinit var binding: FragmentUploadBinding
+    lateinit var cameraPermission: ActivityResultLauncher<String>
+    lateinit var storagePermission: ActivityResultLauncher<String>
+    lateinit var cameraLauncher: ActivityResultLauncher<Uri>
+    lateinit var galleryLauncher: ActivityResultLauncher<String>
 
-//    private val CAMERA = arrayOf(Manifest.permission.CAMERA)
-//    private val STORAGE = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//    private val CAMERA_CODE = 98
-//    private val STORAGE_CODE = 99
-
-    private lateinit var currentPhotoUri: Uri
-    private lateinit var imageCaptureLauncher: ActivityResultLauncher<Intent>
-    private lateinit var imageView: ImageView
-
-//    private val startActivityResult =
-//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-//            if (result.resultCode == Activity.RESULT_OK) {
-//                when (result.data?.extras?.get("requestCode")) {
-//                    CAMERA_CODE -> {
-//                        val bitmap = result.data?.extras?.get("data") as? Bitmap
-//                        bitmap?.let {
-//                            val uri = saveFile(RandomFileName(), "image/jpeg", it)
-//                            binding.avatars.setImageURI(uri)
-//                        }
-//                    }
-//                    STORAGE_CODE -> {
-//                        val uri = result.data?.data
-//                        binding.avatars.setImageURI(uri)
-//                    }
-//                }
-//            }
-//        }
-
-
-//    private fun CallCamera() {
-//        if (checkPermission(CAMERA, CAMERA_CODE) && checkPermission(STORAGE, STORAGE_CODE)) {
-//            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//            imageCaptureLauncher.launch(intent)
-//        }
-//    }
-
-
-
+    var photoUri:Uri? = null
 
 
     override fun onCreateView(
@@ -86,103 +53,67 @@ class UploadFragment : Fragment() {
     ): View? {
         binding = FragmentUploadBinding.inflate(inflater, container, false)
 
-        val view = inflater.inflate(R.layout.fragment_upload, container, false)
-        imageView = view.findViewById(R.id.imageView) // XML에서 ImageView 참조
-
-        // ActivityResultLauncher 초기화
-        imageCaptureLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val bitmap = BitmapFactory.decodeFile(currentPhotoUri.path)
-                imageView.setImageBitmap(bitmap)
+        storagePermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                setViews()
+            } else {
+                Toast.makeText(requireContext(), "권한을 승인해야 앱을 사용할 수 있습니다.",Toast.LENGTH_SHORT).show()
+                requireActivity().finish()
             }
         }
+
+        cameraPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                openCamera()
+            } else {
+                Toast.makeText(requireContext(), "권한을 승인해야 카메라를 사용할 수 있습니다.",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+            if (it) {
+                binding.imagePreview.setImageURI(photoUri)
+            }
+        }
+
+        galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()){
+            binding.imagePreview.setImageURI(it)
+        }
+
+        storagePermission.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
 
         return binding.root
     }
 
-    @Throws(IOException::class)
-    private fun capturePhoto(view: View) {
-        // 카메라 앱 호출을 위한 Intent 생성
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (takePictureIntent.resolveActivity(requireContext().packageManager) != null) {
-            // 이미지 파일 생성
-            val imageFile: File? = createImageFile() // createImageFile()은 사진 파일을 생성하는 함수
-            imageFile?.also {
-                // 이미지가 저장될 파일의 URI 생성
-                currentPhotoUri = FileProvider.getUriForFile(
-                    requireContext(),
-                    "com.example.Z_progject.fileprovider", // FileProvider authority
-                    it
-                )
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, currentPhotoUri)
-                // 카메라 인텐트를 ActivityResultLauncher로 실행
-                imageCaptureLauncher.launch(takePictureIntent)
-            }
-        }
-    }
-    @Throws(IOException::class)
-    private fun createImageFile(): File? {
-        // 이미지 파일 생성 로직을 구현
-        val storageDir: File? = requireContext().getExternalFilesDir(null)
-        return File.createTempFile("IMG_", ".jpg", storageDir)
-    }
-
-
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        binding.camera.setOnClickListener {
-//            CallCamera()
-//        }
-//    }
-
-
-
-    private fun checkPermission(permissions: Array<out String>, type: Int): Boolean {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            for (permission in permissions) {
-                if (ContextCompat.checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(permissions, type)
-                    return false
-                }
-            }
-        }
-        return true
-    }
-
-
-
-
-
-
-
-    private fun saveFile(fileName: String, mimeType: String, bitmap: Bitmap): Uri? {
-        val contentValues = ContentValues().apply {
-            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
-            put(MediaStore.Images.Media.MIME_TYPE, mimeType)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                put(MediaStore.Images.Media.IS_PENDING, 1)
-            }
+    private fun setViews() {
+        binding.cameraBtn.setOnClickListener {
+            cameraPermission.launch(Manifest.permission.CAMERA)
         }
 
-        val uri = requireContext().contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-        uri?.let {
-            val outputStream = requireContext().contentResolver.openOutputStream(it)
-            outputStream?.use { fos ->
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-            }
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                val updatedValues = ContentValues().apply {
-                    put(MediaStore.Images.Media.IS_PENDING, 0)
-                }
-                requireContext().contentResolver.update(it, updatedValues, null, null)
-            }
+        binding.buttonGallery.setOnClickListener {
+            openGallery()
         }
-        return uri
     }
 
-    private fun RandomFileName(): String {
-        return SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).format(System.currentTimeMillis())
+    private fun openCamera() {
+        val photoFile = File.createTempFile(
+            "IMG_",
+            ".jpg",
+            requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        )
+
+        photoUri = FileProvider.getUriForFile(
+            requireContext(), "${requireContext().packageName}.provider", photoFile
+        )
+
+        cameraLauncher.launch(photoUri)
     }
+
+    private fun openGallery() {
+        galleryLauncher.launch("image/*")
+    }
+
+
+
 }
