f91fbbc0379793f03835fbef0b60499fcfd070db
serena <tjgusid@naver.com>
2024-09-22 21:36:54 +0900

    [Feat] 캘린더 일정 dot span 기능 수정(완료x)

--- app/src/main/java/com/example/z_project/MainActivity.kt
+++ app/src/main/java/com/example/z_project/MainActivity.kt
@@ -1,23 +1,14 @@
 package com.example.z_project
 
-import android.net.Uri
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.z_project.chat.ChatFragment
 import com.example.z_project.databinding.ActivityMainBinding
-import com.example.z_project.login.LoginFragment
 import com.example.z_project.mypage.MypageFragment
 import com.example.z_project.record.RecordFragment
 import com.example.z_project.upload.UploadFragment
-import com.example.z_project.mypage.LogoutFragment
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
